96ab534c61a0a24c50b00e1e6c0f3506f3a9e007
DedRobin <pavelrabekin@gmail.com>
2024-01-18 15:25:29 +0300

    feat: task '02-write-file' is done

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const writable = fs.createWriteStream(path.resolve(__dirname, './text.txt'));
+const { stdin, stdout } = process;
+
+function sayGoodbuy() {
+  stdout.write('Good luck learning Node.js!');
+}
+
+const greetings = `
+Greetings!
+Enter a text that you want to write to file: 
+`;
+
+// output 'greetings' message to console
+stdout.write(greetings);
+
+// subcribe to ...
+process.on('exit', sayGoodbuy); // ... enter 'exit'
+process.on('SIGINT', () => process.exit()); //  ... ctrl + c
+
+// subscribe to any data entry
+stdin.on('data', (chunk) => {
+  if (chunk.toString().trimEnd() === 'exit') {
+    // terminate programm if message = 'exit'
+    process.exit();
+  } else {
+    // write data to file
+    writable.write(chunk);
+  }
+});
